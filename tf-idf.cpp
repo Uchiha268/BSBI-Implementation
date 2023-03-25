09fbabfa69b99ed5133524bb8d783cb06f3ad185
Karthik Nair <karthik16092002@gmail.com>
2023-03-25 15:50:44 +0530

    added tf-idf calculator

---
+++ tf-idf.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#define DOC_COUNT 30
+using namespace std;
+
+vector<string> split(string str, string delimiter){
+    vector<string> v;
+    if (!str.empty()) {
+        int start = 0;
+        do{
+            int idx = str.find(delimiter, start);
+            if (idx == string::npos) {
+                break;
+            }
+            int length = idx - start;
+            v.push_back(str.substr(start, length));
+            start += (length + delimiter.size());
+        } while (true);
+        v.push_back(str.substr(start));
+    }
+    return v;
+}
+
+vector<int> getTotalTermsPerDoc(string file){
+    vector<int> tots(DOC_COUNT,0);
+    ifstream fp;
+    fp.open(file);
+    string line;
+    while(getline(fp,line)){
+        vector<string> lineSplit = split(line," ");
+        for(int i = 1;i+1 < lineSplit.size();i+=2){
+            tots[stoi(lineSplit[i])-1]+=stoi(lineSplit[i+1]);
+        }
+    }
+    return tots;
+    
+}
+
+double getIDF(vector<string> s){
+    int wordDocs = (s.size()-1)/2;
+    return log10(DOC_COUNT/wordDocs);
+}
+
+map<int,double> getTF(vector<string> s,vector<int> docTot){
+    map<int,double> tfDoc;
+    for(int i = 1;i+1 < s.size();i+=2){
+        tfDoc[stoi(s[i])] = stod(s[i+1])/docTot[stoi(s[i])-1];
+    }
+    return tfDoc;
+}
+
+
+int main(){
+    vector<int> docTot = getTotalTermsPerDoc("inverted_index.txt");
+
+    // for(int i = 0;i < DOC_COUNT;i++){
+    //     cout << docTot[i] << endl;
+    // }
+
+    // cout << getIDF({"hello","5","6"});
+
+    // vector<string> s = {"hello","5","1234","4","10000"};
+    // map <int,double> tfDoc = getTF(s,docTot);
+    // for(auto i:tfDoc){
+    //     cout << i.first << " " << i.second << endl;
+    // }
+
+    ofstream fout;
+    fout.open("tf_idf.txt");
+    ifstream fp;
+    fp.open("inverted_index.txt");
+    string line;
+    while(getline(fp,line)){
+        vector<string> lineSplit = split(line," ");
+        double IDF = getIDF(lineSplit);
+        map<int,double> TF = getTF(lineSplit,docTot);
+        fout << lineSplit[0] << " " << IDF;
+        for(auto i : TF){
+			fout << " " << i.first << " " << i.second;
+		}
+		fout << endl;
+    }
+    fout.close();
+
+    return 0;
+}
